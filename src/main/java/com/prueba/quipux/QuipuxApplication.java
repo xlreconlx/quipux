48a9de69585077b4721a640c62b0bc13b06dfa7a
andersonur <andersonur@hotmail.com>
2025-07-17 13:57:35 -0500

    primera configuracion

---
+++ src/main/java/com/prueba/quipux/QuipuxApplication.java
@@ -0,0 +1,13 @@
+package com.prueba.quipux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuipuxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuipuxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prueba/quipux/QuipuxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prueba.quipux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuipuxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
